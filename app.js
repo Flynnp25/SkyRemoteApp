edd62d39a5532ee9ce4cf0bddefb63a3a5314172
PADDYSLAPTOP\Paddy <pflynn210392@gmail.com>
2017-08-21 17:35:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var SkyRemote = require('sky-remote');
+
+var remoteControl = new SkyRemote('');
+
+
+function goToChannel(firstDigit,secondDigit,thirdDigit) {
+    remoteControl.press([firstDigit, secondDigit, thirdDigit]);
+}
+
+function pressPower() {
+    remoteControl.press('power');}
+
+    
+// Simple - just send a command
+remoteControl.press('power');
+
+// Cool - send sequences of commands
+//remoteControl.press(['channelup', 'record', 'select']);
+
+// Nice - send commands with a callback
+// remoteControl.press('channelup', function(err) {
+//     if (err) {
+//         console.log("Woah! Something went wrong. Cry time.");
+//     } else {
+//         console.log("I just pressed the Channel Up command.");
+//     };
+// });
